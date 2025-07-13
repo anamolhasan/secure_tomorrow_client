c725ad7ce7e5ed75b10c88a6e619626eb7e19e69
anamolhasan <mdanamolhasan1998@gmail.com>
2025-07-13 20:11:03 +0600

    role base user added

---
+++ src/hooks/useRole.js
@@ -0,0 +1,25 @@
+import useAxiosSecure from './useAxiosSecure'
+import useAuth from './useAuth'
+import { useQuery } from '@tanstack/react-query'
+
+const useRole = () => {
+  const { user, loading } = useAuth()
+  // const [role, setRole] = useState(null)
+  // const [isRoleLoading, setIsRoleLoading] = useState(true)
+  const axiosSecure = useAxiosSecure()
+
+  const { data: role, isLoading: isRoleLoading } = useQuery({
+    queryKey: ['role', user?.email],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/user/role/${user?.email}`)
+      return data
+    },
+  })
+  console.log(role, isRoleLoading)
+  
+
+  return [role?.role, isRoleLoading]
+}
+
+export default useRole
